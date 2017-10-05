fae514e2993cb8212aa53a08cdac0d878bc4629b
Sam Strider <sam@clarity-connect.com>
2017-10-05 12:06:10 -0400

    added favicon

--- contact.php
+++ contact.php
@@ -70,6 +70,8 @@ endif;
     <link href="css/custom.css" rel="stylesheet">
     <link href="//fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i" rel="stylesheet">
 
+    <link rel="SHORTCUT ICON" type="image/gif" href="favicon.gif" />
+
     <script src='https://www.google.com/recaptcha/api.js'></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
